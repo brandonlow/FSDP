34733278f3cfa9ca1f9ab4160c4649e624d6f1bd
64normans <60180087+64normans@users.noreply.github.com>
2021-07-04 18:42:46 +0800

    products admin side
    update and delete function

--- Bookstore 2.0/routes/admin.js
+++ Bookstore 2.0/routes/admin.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 const Admin=require('../models/Admin');
 const bcrypt = require('bcryptjs');
 const admin = require('../models/Admin');
+const Product = require('../models/Product');
 
 router.get('/index',(req,res)=>{
     User.findAll({
@@ -28,10 +29,67 @@ router.get('/tablelist',(req,res)=>{
 	}).catch(err => console.log(err));
 });
 router.get('/producttable',(req,res)=>{
-    res.render('', {layout: "producttable"})
+	Product.findAll({
+		raw: true
+	}).then((products) => {
+		res.render('', {
+            layout:"producttable",
+			products: products
+		});
+	}).catch(err => console.log(err));
 });
-router.get('/addproduct',(req,res)=>{
+router.get('/showaddproduct',(req,res)=>{
     res.render('', {layout: "addproduct"})
+});
+router.post('/addproduct',(req,res)=>{
+	let { title, author, price, url } = req.body;
+	let dateAdded = new Date();
+	Product.create({
+		title,
+		author,
+		price,
+		dateAdded,
+		url
+	})
+	res.redirect('./producttable');
+});
+router.get('/showupdateproduct/:id',(req,res)=>{
+	Product.findOne({
+		where: {
+		id: req.params.id
+		}
+		}).then((product) => {
+			res.render('', {
+				layout:"updateproduct",
+				product: product
+			});
+	}).catch(err => console.log(err));
+});
+router.put('/updateproduct/:id', (req, res) => {
+	let { title, author, price, url } = req.body;
+	Product.update({
+		title: title,
+		author: author,
+		price: price,
+		url: url
+	}, {
+	where: {
+	id: req.params.id
+	}
+	}).then(() => {
+	// After saving, redirect to router.get(/listVideos...) to retrieve all updated
+	// videos
+	res.redirect('../producttable');
+	}).catch(err => console.log(err));
+});
+router.get('/deleteproduct/:id', (req, res) => {
+	Product.destroy({
+		where: {
+			id: req.params.id
+		}
+	}).then(() => {
+		res.redirect('../producttable');
+	}).catch(err => console.log(err));
 });
 module.exports=router;
 
